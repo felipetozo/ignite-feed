fd0e2c3004f0129a3f4859696c4b3e3b4fef1a74
felipetozo <166659635+felipetozo@users.noreply.github.com>
2025-08-29 20:59:15 -0300

    Big update
    Adicionadas fotos que serão usadas no projeto
    Criado Sidebar
    Adicionado React Icons

--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,29 @@
+import styles from './Sidebar.module.css';
+import Image from 'next/image';
+import { LiaUserEditSolid } from "react-icons/lia";
+
 function Sidebar() {
     return (
-        <>
-        </>
+        <aside className={styles.sidebar}>
+            <Image
+                src="/pattern.jpg"
+                width={800}
+                height={300}
+                alt="Ignite Feed"
+            />
+
+            <div className={styles.profile}>
+                <strong>Diego Fernandes</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="/">
+                <LiaUserEditSolid />
+                Editar seu perfil
+                </a>
+            </footer>
+        </aside>
     );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import Image from "next/image";
 import Header from '@/app/components/Header/Header';
+import Sidebar from '@/app/components/Sidebar/Sidebar';
+import styles from './page.module.css';
 
 function Home() {
   return (
     <>
       <Header />
+      <div className={styles.wrapper}>
+          <Sidebar />
+          <main>
+            Main
+          </main>
+      </div>
     </>
   );
 };
