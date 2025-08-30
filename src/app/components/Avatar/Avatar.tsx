f529dee14890b380ebdcaddc7770d7295abbe973
felipetozo <166659635+felipetozo@users.noreply.github.com>
2025-08-30 04:04:04 -0300

    responsividade atualizada

--- src/app/components/Avatar/Avatar.tsx
+++ src/app/components/Avatar/Avatar.tsx
@@ -5,8 +5,8 @@ function Avatar(props: { src: string, hasBorder: boolean; }) {
     return (
         <Image
             src={props.src}
-            width={60}
-            height={60}
+            width={160}
+            height={160}
             alt='Profile Avatar'
             className={props.hasBorder ? styles.avatarWithBorder : styles.avatar }
         />
