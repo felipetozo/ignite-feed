7f30f7ce8223fea235b53acf2bdfa2504dc038c1
felipetozo <166659635+felipetozo@users.noreply.github.com>
2025-08-30 02:16:21 -0300

    component post adicionado

--- src/app/components/Post/Post.tsx
+++ src/app/components/Post/Post.tsx
@@ -1,7 +1,37 @@
+import Image from 'next/image';
+import styles from './Post.module.css';
+import Link from 'next/link';
+
 function Post() {
     return (
-        <>
-        </>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <Image
+                        src="/avatar001.jpg"
+                        width={120}
+                        height={120}
+                        alt="Author Profile"
+                        className={styles.avatar}
+                    />
+                    <div className={styles.authorInfo}>
+                        <strong>Diego Fernandes</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='11 de maio às 08:34' dateTime='2022-05-11 08:34:18'>
+                    Publicado há 1h
+                </time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <Link href="/">jane.design/doctorcare</Link></p>
+                <p>#novoprojeto #nlw #rocketseat</p>
+            </div>
+        </article>
     );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from '@/app/components/Header/Header';
 import Sidebar from '@/app/components/Sidebar/Sidebar';
+import Post from '@/app/components/Post/Post';
 import styles from './page.module.css';
 
 function Home() {
@@ -9,7 +10,14 @@ function Home() {
       <div className={styles.wrapper}>
           <Sidebar />
           <main>
-            Main
+            <Post
+              author="Diego Fernandes"
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda odio aliquam."
+            />
+            <Post
+              author="Gabriel Buzzi"
+              content="Um novo post muito legal"
+            />
           </main>
       </div>
     </>
